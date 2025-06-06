987d2aa3929c4a66bca462e251635bdaf2d2763a
BigChang <119626114+BigChangGame@users.noreply.github.com>
2025-06-06 14:08:52 +0700

    Initial commit

---
+++ js/yeuthich.js
@@ -0,0 +1,74 @@
+const yeuthichdata = [
+     
+      {
+        "title": "Liên Quân Mobile",
+        "version": "1.58.11379483",
+        "image": "https://cdn.glitch.global/a5da2f18-878d-4722-b1ad-5ae2f04638f8/f399da84-52b5-4dfc-82bb-b3a99ce8ce72.image.png?v=1681997379317%27",
+        "image-demo": "https://tiptipios.github.io/ios/IMG_1270.jpeg",
+        "description": "✅Map Full HD Ko che \n ✅Cam xa ( 3 chế độ) \n ✅Hiện until  \n ✅Hiện bổ trợ \n ✅Hiện Avatar \n ✅Hiện rank  \n ✅Hiện lịch sử đấu  \n ✅ Menu Mod Skin,Sảnh (tap 3 ngón tay 2 lần )\n ✅ Kín = Bất tử ",
+        "download_url": "https://drive.google.com/file/d/1Cgh8rsHE0glxJU83e0ilD7WvjThs4Eqj",
+        "updated_at": "2025-05-22",
+        "size": "170 MB",
+        "fake_downloads": 9999999
+    },
+   {
+        "title": "Free Fire",
+        "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/fa/1c/12/fa1c12d5-aa1c-f9ef-2bf4-33c15a87d5a2/AppIcon-1739965179-1x_U007emarketing-0-7-0-85-220-0.png/350x350bb.jpg",
+        "download_url": "FreeFire.html",
+        "fake_downloads": 9999998
+
+    },
+    {
+        "title": "8 Ball Pool",
+        "version": "All Version",
+        "image": "https://play-lh.googleusercontent.com/bPz1guJ6FHF3oIOEy3KqwpaDDKO-hLRaZoyzmM8bLFLN8fWm6L0_EuUnkwv9iqPo3Ag",
+        "image-demo": "https://tiptipios.github.io/ios/IMG_20250516133319551.png",
+        "description": "  \n ✅V1: \n ✅ Tia Dài \n ✅ A Băng \n✅ Bật line khi đề pa để ko bị lỗi\n ✅V2: \n ✅ Chân Nhện 🕷️🕸️\n ✅ A Băng \n ✅ Mở Menu chỉnh tia, Tắt menu để chạm phía sau menu \n ✅Tiêm 💉 Dylib vào game gốc bằng TrollFools",
+        "download_url": "https://oklink.cfd/PrzL",
+        "updated_at": "2025-05-15",
+        "size": "3 MB",
+        "fake_downloads": 9999997
+
+    },  
+ {
+        "title": "Bus Simulator : Ultimate",
+        "version": "2.2.4",
+        "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/25/de/eb/25deeb2c-57c4-d612-36eb-c17f6957998a/AppIcon-1x_U007emarketing-0-8-0-85-220-0.png/350x350bb.jpg",
+        "description": "✅  Vô hạn tài nguyên ( tiền,vàng )\n✅  Unlock Premium\n✅  Free Shop ( only trollstore ) ",
+        "download_url": "https://drive.google.com/file/d/1tXl4WDs6aIEcH8SN-qIrrVta6-AANB9d",
+        "updated_at": "2025-05-10",
+        "size": "1171.29 MB",
+    },
+     {
+        "title": "GTA San Andreas",
+        "image": "https://cdn.glitch.global/a5da2f18-878d-4722-b1ad-5ae2f04638f8/9625f5b6-e3e5-4b99-8965-4978f55d7b93.image.png?v=1682058267059%27",
+        "download_url": "GTA.html",
+        "fake_downloads": 9999995
+
+    },
+     {
+        "title": "The Spike",
+        "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/03/bd/ff/03bdff0f-b915-c3ff-c838-949a1a1eca14/AppIcon-0-0-1x_U007emarketing-0-8-0-0-85-220.png/512x512bb.jpg",
+        "download_url": "thespike.html",
+        "fake_downloads": 9999996
+
+    },
+        {
+        "title": "Soul Knight",
+        "version": "7.2.0.",
+        "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/b8/6c/54/b86c5496-4dba-f117-8bb8-a8943cb7ddd5/AppIcon-0-0-1x_U007emarketing-0-7-0-85-220.png/100x100bb.jpg",
+        "description": "✅ Vô hạn kim cương\n✅ Vô hạn cá\n✅ Vô hạn năng lượng\n✅ Bất tử\n✅ One hit\n✅ Không thời gian hồi chiêu\n✅ Tăng tốc bắn",
+        "download_url": "https://drive.google.com/file/d/1xLQDi9tEFDVYFIF3thcwlH_bfHG5bBI0",
+        "updated_at": "2025-05-01",
+        "size": "502.87 MB",
+    },
+    {
+        "title": "CarX Street",
+        "version": "1.11.0",
+        "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/98/46/92/98469264-32f1-ce97-8c26-0963c092db4e/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/350x350bb.jpg",
+        "description": "✅ Vô hạn tiền tệ ( tăng khi dùng )\n✅ Vô hạn xăng\n✅ Vô hạn nhiên liệu\n✅ Max lever \n✅ Vào tìm kiếm tìm data CarXtreets để ép vô nhé ",
+        "download_url": "https://drive.google.com/file/d/1gnG4-1hZiJ3mjmD8AsWZkz496vhQcTqh",
+        "updated_at": "2025-05-02",
+        "size": "1104.66 MB",
+    }
+];
