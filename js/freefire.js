5ba42fb87593d1ffe917d7bb6eee3f9468493972
BigChang <119626114+BigChangGame@users.noreply.github.com>
2025-06-30 19:05:01 +0700

    Update freefire.js

--- js/freefire.js
+++ js/freefire.js
@@ -1,12 +1,12 @@
 const freefiredata = [
         {
-        "title": "Đợi Up Phiên bản mưới ! ",
-        "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/38/db/07/38db072e-1c90-9d66-3733-ddcda5a25a78/AppIcon-1739965646-1x_U007emarketing-0-7-0-85-220-0.png/350x350bb.jpg",
-        "image-demo": "https://i.imgur.com/j5paRjK.jpeg",
-        "description": "✅",
-        "download_url": "https://t.me/",
-        "version": "1.109.1",
-        "updated_at": "2024-08-14",
+        "title": "FreeFire Menu",
+        "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/00/f0/88/00f08867-9cfc-7718-fb66-b829298dc7ea/AppIcon-1747240258-1x_U007emarketing-0-8-0-85-220-0.png/434x0w.webp",
+        "image-demo": "https://i.imgur.com/o21QUcT.jpeg",
+        "description": "✅Deb Menu Không Key",
+        "download_url": "https://yeumoney.com/wVmOzO",
+        "version": "1.111.1",
+        "updated_at": "2025-6-14",
         "size": "... MB "
     },
 ];
