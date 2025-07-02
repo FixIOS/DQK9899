956de8beff36daa26c7f38ba7d0e5ad3c3e2d093
BigChang <119626114+BigChangGame@users.noreply.github.com>
2025-07-02 14:05:13 +0700

    Update gta.js

--- js/gta.js
+++ js/gta.js
@@ -1,4 +1,14 @@
 const gtadata = [
+           {
+        "title": "FreeFire Max",
+        "image": "https://play-lh.googleusercontent.com/Odw8BGugaJLdbaSbCeZWbTE3Qz1wTiQ0Tsn9nzpoQdnkzWb-gaI58zzTmYDvGpdYKg=w480-h960",
+        "image-demo": "https://i.imgur.com/w76PcsQ.jpeg",
+        "description": "✅ESP \n✅Aimbot \n✅Bypass Fix ban \n✅Fake Lag \n✅Antiban \n✅Antiban(Phòng) \n✅Antiban(Tử chiến) \n✅Antiban(Huấn Luyện) ",
+        "download_url": "https://yeumoney.com/0YipNF_7",
+        "version": "2.113.1",
+        "updated_at": "2025-7-2",
+        "size": "... MB "
+    },
         {
         "title": "Back Up GTA ( dân tổ,naruto )",
         "image": "https://tiptipios.github.io/chanthuhoi/index/b3.jpg",
