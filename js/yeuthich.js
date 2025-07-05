13b15a46f157fd9de29e143094f9ed0bbf7d44ae
BigChang <119626114+BigChangGame@users.noreply.github.com>
2025-07-05 12:14:57 +0700

    Update yeuthich.js

--- js/yeuthich.js
+++ js/yeuthich.js
@@ -48,7 +48,7 @@ const yeuthichdata = [
         "title": "Call of Duty - VN",
         "version": "1.8.51",
         "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/08/9b/5d/089b5d63-af81-cde6-2a06-f0760740fa96/AppIcon-1x_U007emarketing-0-10-0-85-220-0.png/434x0w.webp",
-        "image-demo": "https://i.imgur.com/OKY5ZMC.jpeg",
+        "image-demo": "https://i.imgur.com/TS3b6sA.jpeg",
         "description": "✅Menu Full chức Năng \n✅ESP \n✅Chạy Nhanh\n✅ Aimbot \n✅ Antiban 90% \n✅ Fix Văng ",
         "download_url": "https://yeumoney.com/jTn771j",
         "updated_at": "2025-07-05",
